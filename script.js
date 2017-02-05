7ef1fb00280e5e085e3c292a4b8a38bfaa4fda8a
oopzzozzo <ek3ru8m4@gmail.com>
2017-02-05 23:46:53 +0800

    the one first shown

--- script.js
+++ script.js
@@ -1,5 +1,75 @@
-var t1 = document.getElementById("t1");
-document.getElementById("t1").style.style = "display:none";
-t1.click(function(){
-	t1.style.display = "none";
-	});
+window.onload = function(){
+var counter = 9;
+document.getElementById("t1").onclick = function(){
+	counter = counter-1;
+	this.style.display = "none";
+	if( counter < 1 ){
+		document.getElementById("ans").innerHTML = "only one earth";
+	}
+};
+document.getElementById("t2").onclick = function(){
+	counter = counter-1;
+	this.style.display = "none";
+	if( counter < 1 ){
+		document.getElementById("ans").innerHTML = "only one earth";
+	}
+};
+document.getElementById("t3").onclick = function(){
+	counter = counter-1;
+	this.style.display = "none";
+	if( counter < 1 ){
+		document.getElementById("ans").innerHTML = "only one earth";
+	}
+};
+document.getElementById("t4").onclick = function(){
+	counter = counter-1;
+	this.style.display = "none";
+	if( counter < 1 ){
+		document.getElementById("ans").innerHTML = "only one earth";
+	}
+};
+document.getElementById("t5").onclick = function(){
+	counter = counter-1;
+	this.style.display = "none";
+	if( counter < 1 ){
+		document.getElementById("ans").innerHTML = "only one earth";
+	}
+};
+document.getElementById("t6").onclick = function(){
+	counter = counter-1;
+	this.style.display = "none";
+	if( counter < 1 ){
+		document.getElementById("ans").innerHTML = "only one earth";
+	}
+};
+document.getElementById("t7").onclick = function(){
+	counter = counter-1;
+	this.style.display = "none";
+	if( counter < 1 ){
+		document.getElementById("ans").innerHTML = "only one earth";
+	}
+};
+document.getElementById("t8").onclick = function(){
+	counter = counter-1;
+	this.style.display = "none";
+	if( counter < 1 ){
+		document.getElementById("ans").innerHTML = "only one earth";
+	}
+};
+document.getElementById("t9").onclick = function(){
+	counter = counter-1;
+	this.style.display = "none";
+	if( counter < 1 ){
+		document.getElementById("ans").innerHTML = "only one earth";
+	}
+};
+document.getElementById("c1").onclick = function(){
+	document.getElementById("c1s").play();
+};
+document.getElementById("c2").onclick = function(){
+	document.getElementById("c1s").play();
+};
+document.getElementById("c3").onclick = function(){
+	document.getElementById("c3s").play();
+};
+}
